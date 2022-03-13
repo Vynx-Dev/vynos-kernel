52f7abfb5831b189a883dc8444c5a184a7dbc4fb
Anton Ego <viyaanchauhan@hotmail.com>
2022-03-13 18:27:04 -0400

    new update

---
+++ src/impl/x86_64/boot/main64.asm
@@ -0,0 +1,13 @@
+global long_mode_start
+section .text
+bits 64
+long_mode_start:
+    mov ax, 0
+    mov ss, ax
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    ; DWORD printing OK on the screen
+    mov dword [0xb8000], 0x2f4b2f4f
+    hlt
