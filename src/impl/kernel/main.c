5e8a0270de2d268cc82b5fb4b0b17468b1a54d7d
Lucas Lindsey <badlou101@pm.me>
2022-03-26 23:39:41 -0700

    Implement Stack Smashing Protection

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,4 +1,25 @@
+#include <stdint.h>
+#include <stdlib.h>
 #include "print.h"
+
+#if UINT32_MAX == UINTPTR_MAX
+#define STACK_CHK_GUARD 0xe2dae263
+#else
+#define STACK_CHK_GUARD 0x599e9fbd94fdf737
+#endif
+ 
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+ 
+__attribute__((noreturn))
+void __stack_chk_fail(void)
+{
+#if __STDC_HOSTED__
+	abort();
+#elif __is_myos_kernel
+	panic("Panic 0 Stack Smash");
+#endif
+}
+
 void kernel_main(){
     print_clear();
     print_set_color(PRINT_COLOR_WHITE, PRINT_COLOR_GREEN);// First declare the background of the text, then the color of the text itself.
